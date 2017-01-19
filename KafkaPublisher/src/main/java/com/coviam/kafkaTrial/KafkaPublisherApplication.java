413d9cb50de34edc7fe255e525aa807bc02ca86b
sankalpjohri94 <sankalp.johri@iiitb.org>
2017-01-19 19:15:11 +0530

    Created Project

---
+++ KafkaPublisher/src/main/java/com/coviam/kafkaTrial/KafkaPublisherApplication.java
@@ -0,0 +1,12 @@
+package com.coviam.kafkaTrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaPublisherApplication.class, args);
+	}
+}
---
+++ KafkaSubscriber/src/main/java/com/coviam/kafkaTrial/KafkaSubscriberApplication.java
@@ -0,0 +1,12 @@
+package com.coviam.kafkaTrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSubscriberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSubscriberApplication.class, args);
+	}
+}
